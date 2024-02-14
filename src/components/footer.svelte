3c3c09091b3e9ebe14682d94842d6003df6c3f02
rightpair <tkddn204@gmail.com>
2024-02-14 14:54:03 +0900

    :sparkles: Footer 추가

---
+++ src/components/footer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<footer class="bg-white dark:bg-gray-900">
+  <div class="container px-4 py-8 mx-auto">
+    <hr class="my-10 border-gray-200 dark:border-gray-700" />
+
+    <div class="flex flex-col items-center sm:flex-row sm:justify-between">
+      <p class="text-sm text-gray-500">© Copyright 2024. Rightpair All Rights Reserved.</p>
+    </div>
+  </div>
+</footer>
