11ad49eae13fab3a4b134106195ed840acc2304c
rightpair <tkddn204@gmail.com>
2024-02-14 16:15:29 +0900

    :lipstick: hero 및 project의 css 변경

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -4,18 +4,18 @@
 </script>
 
 <section id="hero" class="bg-emerald-50 dark:bg-gray-900">
-  <div class="mx-auto grid max-w-2xl items-center gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+  <div class="mx-auto grid grid-flow-row md:grid-flow-col items-center gap-y-16 px-4 py-24 lg:mt-12 sm:py-32 lg:max-w-7xl lg:px-8">
 
     <div class="m-3 lg:mt-0 lg:col-span-3 lg:flex">
       <img src="https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F6fff5f0a-b9dd-4c74-8f5d-f76aabefe27e%2Ff2ea367e-516a-4907-b92a-5c6a6da0a5cb%2Fphoto_2020-09-03_00-17-53.jpg?table=block&id=06de89e4-fd6c-471f-bf7b-01943c90fbbd&spaceId=6fff5f0a-b9dd-4c74-8f5d-f76aabefe27e&width=250&userId=590c4dcc-2370-4f43-b1ae-eb4e4f98da83&cache=v2" alt="mockup">
     </div>
-    <div class="mr-auto place-self-center lg:col-span-7">
+    <div class="w-full mr-auto place-self-center lg:col-span-7">
       <h1 class="max-w-5xl mb-4 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl dark:text-white">
         <span class="mb-5">👋 안녕하세요!</span>
       </h1>
       <p class="text-2xl mb-10 leading-loose">
         주니어 <span class="text-4xl font-bold underline decoration-sky-500">웹 백엔드</span> 개발자,
-        <br class="md:hidden">
+        <br class="sm:hidden">
         <span class="text-5xl font-bold bg-yellow-200">한상우</span>입니다.
       </p>
       <hr class="bg-black mx-auto h-0.5" />
--- src/components/project.svelte
+++ src/components/project.svelte
@@ -3,15 +3,15 @@
   import projectData from '$lib/data/projectData.json';
 </script>
 
-<section id="project" class="bg-gray-50 dark:bg-gray-900">
-  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:pt-16 lg:pb-8">
+<section id="project" class="pt-12 bg-gray-50 dark:bg-gray-900">
+  <div class="grid max-w-screen-xl px-4 py-4 mx-auto lg:pt-16 lg:pb-8">
     <div class="mr-auto place-self-center lg:col-span-7">
       <h2 class="max-w-5xl mb-4 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl dark:text-white">
         <span class="mb-3">🖼️ 프로젝트</span>
       </h2>
     </div>
   </div>
-  <hr class="bg-black max-w-screen-xl mx-auto h-0.5" />
+  <hr class="bg-black max-w-screen-xl mx-5 h-0.5" />
   <div class="block pt-12 md:flex">
     <div class="flex-none text-lg text-gray-600 font-light md:flex-1 md:pl-20 md:pr-20">
       {#each projectData as data}
